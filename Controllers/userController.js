fd85171bbd6b0843e85e62a20a55a80e148a13ff
Adnank999 <adnanhossain4ever@gmail.com>
2024-08-04 23:07:23 +0600

    added email validation

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -7,29 +7,37 @@ const jwt = require('jsonwebtoken')
 const jwtSecret = process.env.JWT_SECRET;
 // const { PrismaClient } = require("@prisma/client");
 
-
+const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+};
 const createUser = asyncHandler(async (req, res) => {
-    let { email, password } = req.body;
+    let { name, email, password } = req.body;
 
     try {
+       
+        if (!validateEmail(email)) {
+            return res.status(400).send({ message: "Invalid email format" });
+        }
+
         const userExists = await prisma.user.findUnique({
             where: { email: email }
         });
 
         if (!userExists) {
-            // Hash the password before saving the user
             const saltRounds = 10;
             const hashedPassword = await bcrypt.hash(password, saltRounds);
 
             // Create the user with the hashed password
             const user = await prisma.user.create({
                 data: {
-                    ...req.body,
+                    name, 
+                    email,
                     password: hashedPassword
                 }
             });
 
-            res.status(200).send({ message: "User Created Successfully", user });
+            res.status(201).send({ message: "User Created Successfully", user });
         } else {
             res.status(409).send({ message: "User already registered with this email" });
         }
@@ -37,7 +45,6 @@ const createUser = asyncHandler(async (req, res) => {
         res.status(500).send({ message: "Internal Server Error", error: error.message });
     }
 });
-
 const login = asyncHandler(async(req,res)=>{
     const {email,password} = req.body;
     let user = await prisma.user.findFirst({where: {email:email}})
